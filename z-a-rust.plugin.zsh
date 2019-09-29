8d1c4f739dc5d401ed0d863e0b6b854241cd1aba
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-09-29 23:58:22 +0200

    plugin.zsh,*atclone-handler: Register the hooks and a first atclone hook

---
+++ z-a-rust.plugin.zsh
@@ -0,0 +1,40 @@
+# Copyright (c) 2019 Sebastian Gniazdowski
+# License MIT
+
+
+# According to the Zsh Plugin Standard:
+# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+autoload .za-rust-bin-or-src-function-body \
+    .za-rust-mod-function-body \
+    :za-rust-atload-handler :za-rust-atclone-handler \
+    :za-rust-atpull-handler :za-rust-help-handler \
+    :za-rust-atdelete-handler
+
+# An empty stub to fill the help handler fields
+:za-rust-help-null-handler() { :; }
+
+@zplg-register-annex "z-a-rust" \
+    hook:atload \
+    :za-rust-atload-handler \
+    :za-rust-help-handler \
+    "rustup''|cargo''" # also register new ices
+
+@zplg-register-annex "z-a-rust" \
+    hook:atclone \
+    :za-rust-atclone-handler \
+    :za-rust-help-null-handler
+
+@zplg-register-annex "z-a-rust" \
+    hook:\%atpull \
+    :za-rust-atclone-handler \
+    :za-rust-help-null-handler
+
+@zplg-register-annex "z-a-rust" \
+    hook:atdelete \
+    :za-rust-atdelete-handler \
+    :za-rust-help-null-handler
+
