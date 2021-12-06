9922e4058d54ec59eece8aebfe500d11c33d1246
Salvydas Lukosius <sall@w-ss.io>
2021-12-06 08:45:34 +0000

    modified:   .za-rust-bin-or-src-function-body 	modified:   z-a-rust.plugin.zsh 	renamed:    "\342\206\222za-rust-atclone-handler" -> za-rust-atclone-handler 	renamed:    "\342\206\222za-rust-atdelete-handler" -> za-rust-atdelete-handler 	renamed:    "\342\206\222za-rust-atload-handler" -> za-rust-atload-handler 	renamed:    "\342\206\222za-rust-atpull-handler" -> za-rust-atpull-handler 	renamed:    "\342\206\222za-rust-help-handler" -> za-rust-help-handler

--- z-a-rust.plugin.zsh
+++ z-a-rust.plugin.zsh
@@ -2,37 +2,36 @@
 # Copyright (c) 2021 Z-Shell ZI Contributors
 #
 # According to the Zsh Plugin Standard:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+# https://github.com/z-shell/zi/wiki/Zsh-Plugin-Standard
 
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 autoload .za-rust-bin-or-src-function-body \
-    →za-rust-atload-handler →za-rust-atclone-handler \
-    →za-rust-atpull-handler →za-rust-help-handler \
-    →za-rust-atdelete-handler
+za-rust-atload-handler za-rust-atclone-handler \
+za-rust-atpull-handler za-rust-help-handler \
+za-rust-atdelete-handler
 
 # An empty stub to fill the help handler fields
-→za-rust-help-null-handler() { :; }
+za-rust-help-null-handler() { :; }
 
 @zi-register-annex "z-a-rust" \
-    hook:atload-40 \
-    →za-rust-atload-handler \
-    →za-rust-help-handler \
-    "rustup|cargo''" # also register new ices
+hook:atload-40 \
+za-rust-atload-handler \
+za-rust-help-handler \
+"rustup|cargo''" # also register new ices
 
 @zi-register-annex "z-a-rust" \
-    hook:atclone-40 \
-    →za-rust-atclone-handler \
-    →za-rust-help-null-handler
+hook:atclone-40 \
+za-rust-atclone-handler \
+za-rust-help-null-handler
 
 @zi-register-annex "z-a-rust" \
-    hook:\%atpull-40 \
-    →za-rust-atclone-handler \
-    →za-rust-help-null-handler
+hook:\%atpull-40 \
+za-rust-atclone-handler \
+za-rust-help-null-handler
 
 @zi-register-annex "z-a-rust" \
-    hook:atdelete-40 \
-    →za-rust-atdelete-handler \
-    →za-rust-help-null-handler
-
+hook:atdelete-40 \
+za-rust-atdelete-handler \
+za-rust-help-null-handler
