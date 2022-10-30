edb1e86284e6233babca2e7404cc741bc94343cc
Salvydas Lukosius <sall@w-ss.io>
2022-10-30 14:50:25 +0000

    Update z-a-rust.plugin.zsh
    Signed-off-by: Salvydas Lukosius <sall@w-ss.io>

--- z-a-rust.plugin.zsh
+++ z-a-rust.plugin.zsh
@@ -4,16 +4,16 @@
 # Copyright (c) 2021 Z-Shell Community
 #
 # According to the Zsh Plugin Standard:
-# https://z.digitalclouds.dev/community/zsh_plugin_standard/#zero-handling
+# https://wiki.zshell.dev/community/zsh_plugin_standard/#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-# https://z.digitalclouds.dev/community/zsh_plugin_standard/#funtions-directory
+# https://wiki.zshell.dev/community/zsh_plugin_standard/#funtions-directory
 if [[ $PMSPEC != *f* ]] {
   fpath+=( "${0:h}/functions" )
 }
 
-# https://z.digitalclouds.dev/community/zsh_plugin_standard/#the-proposed-function-name-prefixes
+# https://wiki.zshell.dev/community/zsh_plugin_standard/#the-proposed-function-name-prefixes
 autoload -Uz .za-rust-bin-or-src-function-body .z-a-rust-download-file-stdout \
 →za-rust-atload-handler →za-rust-atclone-handler \
 →za-rust-atpull-handler →za-rust-help-handler \
